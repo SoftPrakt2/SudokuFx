9c4e385afa29afe086dcc2d555bb065452a8d5bb
Lukers7 <80413586+Lukers7@users.noreply.github.com>
2021-05-10 17:04:55 +0200

    testcommit

--- SudokuPrSoftProjekt/src/test/java/test/GUITest.java
+++ SudokuPrSoftProjekt/src/test/java/test/GUITest.java
@@ -1,33 +1,73 @@
 package test;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
-import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.framework.junit5.Start;
 
-import application.BasicGameBuilder;
-import application.MainMenu;
-import application.SudokuGameBuilder;
-import controller.GameController;
-import javafx.event.ActionEvent;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import logic.Gamestate;
-import logic.SudokuLogic;
 
 @ExtendWith(ApplicationExtension.class)
-public class GUITest  {
+  class GUITest  {
+	
+	//private Button button;
 	
 	
 	
 	
-//
-//
-//	    @Test
-//	    public void test() {
-//	    	GameController mainMenu = new GameController(new SudokuGameBuilder(),  new SudokuLogic(Gamestate.OPEN, 0, 0, false));
-//	    	mainMenu.calculateGameTime();
-//	    //	mainMenu.createGame(3);
-//	    }
+    /**
+     * Will be called with {@code @Before} semantics, i. e. before each test method.
+     *
+     * @param stage - Will be injected by the test runner.
+     */
+	
+	@BeforeAll
+	public static void setupSpec() throws Exception {
+	
+	
+	            System.setProperty("testfx.robot", "glass");
+	         
+	            System.setProperty("prism.text", "t2k");
+	            System.setProperty("java.awt.headless", "true");
+	         //   FxToolkit.registerPrimaryStage();
+	        }
+	    
+	  
+	private Button button;
+
+    /**
+     * Will be called with {@code @Before} semantics, i. e. before each test method.
+     *
+     * @param stage - Will be injected by the test runner.
+     */
+    @Start
+    private void start(Stage stage) {
+        button = new Button("click me!");
+        button.setId("myButton");
+        button.setOnAction(actionEvent -> button.setText("clicked!"));
+     
+    }
+
+    /**
+     * @param robot - Will be injected by the test runner.
+     */
+    @Test
+    void should_contain_button_with_text(FxRobot robot) {
+        Assertions.assertThat(button).hasText("click me!");
+        // or (lookup by css id):
+       
+    }
+
+   /**
+    * @param robot - Will be injected by the test runner.
+    */
+   
 }
+
